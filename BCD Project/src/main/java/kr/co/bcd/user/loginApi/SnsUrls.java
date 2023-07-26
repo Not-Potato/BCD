5ec335a9a1d524d7c2dfbf348bb1a9b48b2649cc
ssh <bko2369@naver.com>
2023-07-26 09:51:34 +0900

    [2023.7.26] 9:53 서수현 / websocket config, handler 생성

--- BCD Project/src/main/java/kr/co/bcd/user/loginApi/SnsUrls.java
+++
@@ -1,9 +0,0 @@
-package kr.co.bcd.user.loginApi;
-
-public interface SnsUrls {
-	static final String NAVER_ACCESS_TOKEN = "https://nid.naver.com/oath2.0/token/grant_type=authorization_code"; 
-	static final String NAVER_AUTH = "https://nid.naver.com/oauth2.0/authorize";
-	
-	static final String NAVER_PROFILE_URL = "https://openapi.naver.com/v1/nid/me";
-	
-}
